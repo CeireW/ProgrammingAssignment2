2aeb4a8ba253cdd4043430363a4b5f567abfdcd3
CeireW <68636968+CeireW@users.noreply.github.com>
2020-08-04 14:13:14 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+setwd('C:/Users/ceire/Documents/R Course')
+##
+## I set input x as a matrix
+## I set inversed value i to null
+## I changed references of mean to inversed
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInversed <- function(solveMatrix) i <<- solveMatrix
+  getInversed <- function() i
+  list(set = set, get = get, setInversed = setInversed, getInversed = getInversed)
 }
-
-
-## Write a short comment describing this function
-
+##
+## Here I changed mean to invedrsed and m to i
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getInversed()
+  if(!is.null(i)) {
+    message("getting cached inversed matrix")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setInversed(i)
+  i
 }
